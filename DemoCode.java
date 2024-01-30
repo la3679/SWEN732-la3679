35b5f9cddb35db969dd61c392095549351bbdade
la3679 <143540361+la3679@users.noreply.github.com>
2024-01-30 13:57:10 -0500

    Create DemoCode.java
    added a demo code

---
+++ DemoCode.java
@@ -0,0 +1,11 @@
+public class HomeworkDescription {
+    public static void main(String[] args) {
+        System.out.println("Hi there, I'm Love Jayesh Ahir. I have a good technical skill set, with experience in Python, Java with Spring Boot and REST API, C++, C and Oracle SQL among other programming languages. In addition, I have knowledge of operating systems like Windows and Linux, as well as tools and programs like Visual Studio, Eclipse, Git/GitHub, and web development languages like HTML, CSS, JavaScript, and JQuery.\n");
+        System.out.println("In terms of leadership, I have actively participated in code reviews and given helpful criticism to foster cooperative, quality-focused work environments. Additionally, I have contributed significantly to front-end project development, using tools like JavaScript, HTML, CSS, and Bootstrap to build user-friendly and responsive user interfaces.\n");
+        System.out.println("I am dedicated to continuous learning, proactive in seeking out relevant online courses, and committed to delivering high-quality work in software engineering and web development.\n");
+        System.out.println("The people I want to be in a team with is:");
+        System.out.println("1. Shridhar Vilas Shinde (ss7536@g.rit.edu)");
+        System.out.println("2. Kush Jayesh Ahir (ka8540@g.rit.edu)");
+        System.out.println("3. Bharathi Pandurangan(bp6191@rit.edu)");
+    }
+}
